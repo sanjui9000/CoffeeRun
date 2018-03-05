0cd12f771f91616bd4ce6772ac49cf1b8d652f88
sanjui9000 <sanjui9000@gmail.com>
2018-03-05 15:59:07 -0800

    Initial Commit
    1. Implemented Chapter 8 through Chapter 11.
    2. Added respective plugins- jQuery, jQuery Modal etc..
    3. Fixed all ESLint issues and few CSSLint issues.
    4. Made sure that output looks same as book mentions.
    5. Ensured that all the functions as mentioned by professor have been implemented.
    6. Code reformatted using atom-beautify.

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+(function(window) {
+  "use strict";
+  var $ = window.jQuery;
+  var FORM_SELECTOR = "[data-coffee-order='form']";
+  var CHECKLIST_SELECTOR = "[data-coffee-order='checklist']";
+  var App = window.App;
+  var Truck = App.Truck;
+  var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+  var CheckList = App.CheckList;
+  var myTruck = new Truck("Serenity", new DataStore());
+  window.myTruck = myTruck;
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+  var formHandler = new FormHandler(FORM_SELECTOR);
+  console.log(formHandler);
+
+  // Handle reset to change strength element span and label back to defaults
+  $("#resetButton").click(function() {
+    var sliderOutput = document.getElementById("strengthOutput");
+    var sliderLabel = document.getElementById("strengthLabel");
+    sliderOutput.textContent = 30;
+    sliderOutput.style.color = "green";
+    sliderLabel.style.color = "green";
+  });
+
+  formHandler.addSubmitHandler(function(data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
+})(window);
